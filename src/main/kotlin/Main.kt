a56213e71e3f64661d2dc0bdf292026a2f2a4c9c
manish <manish.kumar3@girnarsoft.com>
2024-07-28 13:17:07 +0530

    working on rebase

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,17 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+open class GitTutorial {
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+
+    companion object{
+        @JvmStatic
+        fun main(args: Array<String>) {
+            var rebase:String="rebase"
+            println("Hello World!")
+
+            // Try adding program arguments via Run/Debug configuration.
+            // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+            println("Program arguments:$rebase")
+        }
+    }
+
 }
+
